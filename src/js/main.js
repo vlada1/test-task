66ac27875fb3ea0898d7535c6d3cb83bcdb515e2
Vlada Andrushchenko <avv771kn@gmail.com>
2017-06-21 01:09:49 +0300

    full markup and responsive

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,10 @@
 //= ../../bower_components/jquery/dist/jquery.min.js
 //= ../../bower_components/owl.carousel/dist/owl.carousel.min.js
-
+//= ../../bower_components/jquery-waypoints/waypoints.min.js
+//= ../../bower_components/jquery.scrollTo-2.1.2/jquery.scrollTo.min.js
 
 /*
  * Custom
  */
 //= partials/script.js
+//= partials/navigation.js
---
+++ src/js/partials/navigation.js
@@ -0,0 +1,38 @@
+$(function() {
+
+    var nav = $(".page-nav");
+
+    var sections = $(".tracked");
+    var navigation_links = $(".page-nav a");
+
+    sections.waypoint({
+        handler: function(event, direction) {
+
+            var active_section;
+            active_section = $(this);
+            if (direction === "up") active_section = active_section.prev();
+
+            var active_link = $('.page-nav a[href="#' + active_section.attr("id") + '"]');
+            navigation_links.removeClass("active");
+            active_link.addClass("active");
+
+        },
+        offset: '10%'
+    });
+
+
+    navigation_links.click(function(event) {
+
+        $.scrollTo(
+            $(this).attr("href"), {
+                duration: 600,
+                offset: {
+                    'left': 0,
+                    'top': -0.15 * $(window).height()
+                }
+            }
+        );
+    });
+
+
+});
